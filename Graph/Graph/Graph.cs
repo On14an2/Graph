75999248cdcdf7035875103deded72622fdab984
on14an <112744129+On14an2@users.noreply.github.com>
2023-06-08 12:06:07 +0700

    Add files via upload

---
+++ Graph/Graph/Graph.cs
@@ -0,0 +1,48 @@
+﻿public class Graph
+{
+    private Dictionary<int, List<int>> adjacencyList;
+
+    public Graph()
+    {
+        adjacencyList = new Dictionary<int, List<int>>();
+    }
+
+    public void AddVertex(int vertex)
+    {
+        if (!adjacencyList.ContainsKey(vertex))
+        {
+            adjacencyList[vertex] = new List<int>();
+        }
+    }
+
+    public void AddEdge(int source, int destination)
+    {
+        if (!adjacencyList.ContainsKey(source))
+        {
+            adjacencyList[source] = new List<int>();
+        }
+
+        if (!adjacencyList.ContainsKey(destination))
+        {
+            adjacencyList[destination] = new List<int>();
+        }
+
+        adjacencyList[source].Add(destination);
+        adjacencyList[destination].Add(source);
+    }
+
+    public void PrintGraph()
+    {
+        foreach (var vertex in adjacencyList)
+        {
+            Console.Write(vertex.Key + ": ");
+
+            foreach (var neighbor in vertex.Value)
+            {
+                Console.Write(neighbor + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Graph/Graph/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Graph graph = new Graph();
+
+        
+        graph.AddVertex(1);
+        graph.AddVertex(2);
+        graph.AddVertex(3);
+        graph.AddVertex(4);
+        graph.AddVertex(5);
+
+        graph.AddEdge(1, 2);
+        graph.AddEdge(1, 3);
+        graph.AddEdge(2, 4);
+        graph.AddEdge(3, 4);
+        graph.AddEdge(4, 5);
+        
+        graph.PrintGraph();
+    }
+}
